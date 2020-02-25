d8d5137b3beb6e304cc1810fb216107d088b1913
puskar <puskar.subedi.2021@student.lhschools.org>
2020-02-25 13:41:10 -0500

    intellij commit

---
+++ src/com/LickingHights/Main.java
@@ -0,0 +1,36 @@
+package com.LickingHights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+--------------------------+");
+        System.out.println("|       Gas Station        |");
+        System.out.println("|         Brand            |");
+        System.out.println("|       Sale Receipt       |");
+        System.out.println("|                          |");
+        System.out.println("| Puskar's gas             |");
+        System.out.println("| 292 Lockmead Drive,      |");
+        System.out.println("| Pataskala, OH  484554    |");
+        System.out.println("|                          |");
+        System.out.println("|   DATE       TIME        |");
+        System.out.println("| 01/01/2020   1:22PM      |");
+        System.out.println("|                          |");
+        System.out.println("| INVOICE#   10101010      |");
+        System.out.println("|  AUTH#                   |");
+        System.out.println("|      Mastercard          |");
+        System.out.println("|    ACCOUNT NUMBER        |");
+        System.out.println("|  ************19          |");
+        System.out.println("|                          |");
+        System.out.println("|  PUMP  PRODUCT $/G       |");
+        System.out.println("|   01    UNLD   $2.90     |");
+        System.out.println("|                          |");
+        System.out.println("|  GALLONS  FUEL  TOTAL    |");
+        System.out.println("|   20.69    $   60.00     |");
+        System.out.println("|                          |");
+        System.out.println("|      THANK YOU!          |");
+        System.out.println("|      GAS STATION         |");
+        System.out.println("|        BRAND             |");
+        System.out.println("|--------------------------|");
+    }
+
+}
